527a29a7e2edfbf3d18e7d709a4d72aad67389b9
Manolo Müller <manolo.mueller@gmail.com>
2021-03-17 15:26:09 +0100

    added audiopluginhost build support and test

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void DemoPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("SALI MITENAND!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void DemoPluginAudioProcessorEditor::resized()
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -24,18 +24,16 @@ DemoPluginAudioProcessor::DemoPluginAudioProcessor()
 {
 }
 
-DemoPluginAudioProcessor::~DemoPluginAudioProcessor()
-{
+DemoPluginAudioProcessor::~DemoPluginAudioProcessor(){
+
 }
 
 //==============================================================================
-const juce::String DemoPluginAudioProcessor::getName() const
-{
+const juce::String DemoPluginAudioProcessor::getName() const{
     return JucePlugin_Name;
 }
 
-bool DemoPluginAudioProcessor::acceptsMidi() const
-{
+bool DemoPluginAudioProcessor::acceptsMidi() const{
    #if JucePlugin_WantsMidiInput
     return true;
    #else
@@ -43,8 +41,7 @@ bool DemoPluginAudioProcessor::acceptsMidi() const
    #endif
 }
 
-bool DemoPluginAudioProcessor::producesMidi() const
-{
+bool DemoPluginAudioProcessor::producesMidi() const{
    #if JucePlugin_ProducesMidiOutput
     return true;
    #else
@@ -52,8 +49,7 @@ bool DemoPluginAudioProcessor::producesMidi() const
    #endif
 }
 
-bool DemoPluginAudioProcessor::isMidiEffect() const
-{
+bool DemoPluginAudioProcessor::isMidiEffect() const{
    #if JucePlugin_IsMidiEffect
     return true;
    #else
@@ -61,51 +57,42 @@ bool DemoPluginAudioProcessor::isMidiEffect() const
    #endif
 }
 
-double DemoPluginAudioProcessor::getTailLengthSeconds() const
-{
+double DemoPluginAudioProcessor::getTailLengthSeconds() const{
     return 0.0;
 }
 
-int DemoPluginAudioProcessor::getNumPrograms()
-{
+int DemoPluginAudioProcessor::getNumPrograms(){
     return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
                 // so this should be at least 1, even if you're not really implementing programs.
 }
 
-int DemoPluginAudioProcessor::getCurrentProgram()
-{
+int DemoPluginAudioProcessor::getCurrentProgram(){
     return 0;
 }
 
-void DemoPluginAudioProcessor::setCurrentProgram (int index)
-{
+void DemoPluginAudioProcessor::setCurrentProgram (int index){
 }
 
-const juce::String DemoPluginAudioProcessor::getProgramName (int index)
-{
+const juce::String DemoPluginAudioProcessor::getProgramName (int index){
     return {};
 }
 
-void DemoPluginAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
+void DemoPluginAudioProcessor::changeProgramName (int index, const juce::String& newName){
 }
 
 //==============================================================================
-void DemoPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
+void DemoPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock){
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
 }
 
-void DemoPluginAudioProcessor::releaseResources()
-{
+void DemoPluginAudioProcessor::releaseResources(){
     // When playback stops, you can use this as an opportunity to free up any
     // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool DemoPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
+bool DemoPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const{
   #if JucePlugin_IsMidiEffect
     juce::ignoreUnused (layouts);
     return true;
@@ -152,38 +139,34 @@ void DemoPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     // interleaved by keeping the same state.
 
     for (int channel = 0; channel < totalNumOutputChannels; ++channel){
-        auto* channelData = buffer.getWritePointer (channel);
+        auto *boi = buffer.getReadPointer(channel);
+        //auto* channelData = buffer.getWritePointer (channel);
     }
 }
 
 //==============================================================================
-bool DemoPluginAudioProcessor::hasEditor() const
-{
+bool DemoPluginAudioProcessor::hasEditor() const{
     return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* DemoPluginAudioProcessor::createEditor()
-{
+juce::AudioProcessorEditor* DemoPluginAudioProcessor::createEditor(){
     return new DemoPluginAudioProcessorEditor (*this);
 }
 
 //==============================================================================
-void DemoPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
-{
+void DemoPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData){
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
 }
 
-void DemoPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
-{
+void DemoPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes){
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter(){
     return new DemoPluginAudioProcessor();
 }
